093d92971767159c54313d47c630dd50b2e5b7d4
Cole Duersch <coleduersch@gmail.com>
2018-10-09 08:17:52 -0600

    Fun Factorials

---
+++ Main.java
@@ -0,0 +1,27 @@
+package com.github.cdtommy;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Enter a number");
+        static int factorial(int n);
+        Scanner input = new Scanner(System.in);
+
+        if (input.hasNextInt()){
+            var n = input.nextInt();
+            input.nextInt();
+            while (n > 1 ){
+                System.out.println(n*(n-1));
+                n++;
+                return;
+            }
+
+
+
+        }
+    }
+}
+
+
